b058d918ca52450f91510ea792eda03ea1e75a23
François Pottier <francois.pottier@inria.fr>
2026-02-17 09:17:41 +0100

    bool.v: more lemmas.

--- src/bool.v
+++ src/bool.v
@@ -1,5 +1,11 @@
 From Stdlib Require Import Utf8.
 
+Lemma bool_neg b :
+  b = false ↔ ¬ (b = true).
+Proof.
+  destruct b; split; congruence.
+Qed.
+
 Definition isBool (b : bool) (P : Prop) :=
   b = true ↔ P.
 
@@ -8,5 +14,22 @@ Lemma isBool_conseq b P P' :
   (P ↔ P') →
   isBool b P'.
 Proof.
+  unfold isBool. tauto.
+Qed.
+
+Lemma isBool_elim b P :
+  isBool b P →
+  b = true →
+  P.
+Proof.
+  unfold isBool. tauto.
+Qed.
+
+Lemma isBool_elim_neg b P :
+  isBool b P →
+  b = false →
+  ¬ P.
+Proof.
+  generalize (bool_neg b).
   unfold isBool. tauto.
 Qed.
