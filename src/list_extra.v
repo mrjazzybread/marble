5b3ec9115c0440639cc64dbbe851d35885c4a7c1
François Pottier <francois.pottier@inria.fr>
2026-02-26 15:04:18 +0100

    A lot more results about lists.

--- src/list_extra.v
+++ src/list_extra.v
@@ -4,6 +4,42 @@ Unset Universe Minimization ToSet.
 Generalizable All Variables.
 Set Universe Polymorphism.
 
+Lemma list_lookup_insert_eq' {A} (xs : list A) i j x :
+  i = j →
+  i < length xs →
+  <[i:=x]>xs !! j = Some x.
+Proof.
+  intros. subst. rewrite list_lookup_insert_eq by eauto. eauto.
+Qed.
+
+Lemma list_lookup_total_insert_eq' `{Inhabited A} (xs : list A) i j x :
+  i = j →
+  i < length xs →
+  <[i:=x]>xs !!! j = x.
+Proof.
+  intros. subst. rewrite list_lookup_total_insert_eq by eauto. eauto.
+Qed.
+
+Global Instance singleton_list {A} : Singleton A (list A) :=
+  λ x, [x].
+
+Lemma length_singleton {A} (x : A) : length {[x]} = 1.
+Proof. reflexivity. Qed.
+
+Lemma lookup_singleton_eq_0 {A} i (x : A) :
+  i = 0 →
+  ({[x]} : list A) !! i = Some x.
+Proof.
+  intros. subst. reflexivity.
+Qed.
+
+Lemma lookup_total_singleton_eq_0 `{Inhabited A} i (x : A) :
+  i = 0 →
+  ({[x]} : list A) !!! i = x.
+Proof.
+  intros. subst. reflexivity.
+Qed.
+
 Lemma lookup_app' {A} (xs ys : list A) i :
   (xs ++ ys) !! i =
     if decide (i < length xs) then xs !! i
@@ -66,11 +102,14 @@ Lemma sub_diag' (x y : nat) :
 Proof. lia. Qed.
 
 Global Hint Rewrite
-  Nat.sub_0_l Nat.sub_0_r Nat.sub_diag sub_succ
+  Nat.sub_0_l Nat.sub_0_r
+  Nat.sub_diag sub_succ
+  Nat.add_simpl_l Nat.add_simpl_r
   @app_nil_l @app_nil_r
   @replicate_0
   @length_nil
   @length_cons
+  @length_singleton
   @length_app
   @length_insert
   @length_replicate
@@ -104,13 +143,21 @@ Global Hint Extern 1 (_ < List.length _) => (list; lia) : lia.
 Global Hint Rewrite
   @lookup_total_cons_eq_0
   @lookup_total_cons_ne_0
-  using lia
+  @list_lookup_insert_eq'
+  @list_lookup_total_insert_eq'
+  @list_lookup_insert_ne
+  @list_lookup_total_insert_ne
+  @lookup_replicate_2
+  @lookup_total_replicate_2
+  using (list; lia)
 : list.
 
 Global Hint Rewrite
   @insert_app_l
   @insert_app_r_alt
   @insert_replicate_0
+  @take_app
+  @drop_app
   using (list; lia)
 : list.
 
@@ -134,6 +181,8 @@ Global Hint Rewrite
   @take_ge
   @drop_0'
   @drop_ge
+  @lookup_singleton_eq_0
+  @lookup_total_singleton_eq_0
   @lookup_take_lt
   @lookup_drop
   using (list; lia)
@@ -151,17 +200,26 @@ Global Ltac apply_prefix_length :=
     list in h'
   end.
 
-(* TODO should we provide a variant that uses total lookups? *)
 Lemma list_eq_same_length' {A} (xs ys : list A) :
   length xs = length ys →
   (∀ i, i < length xs → xs !! i = ys !! i) →
   xs = ys.
 Proof.
-  intros ? Heq.
+  intros.
   apply list_eq_same_length with (n := length xs); eauto 1.
   intros i x y Hi Hxs Hys.
   cut (xs !! i = ys !! i). { congruence. } clear Hxs Hys.
   eauto.
+Qed.
+
+Lemma list_eq_same_length'_total `{Inhabited A} (xs ys : list A) :
+  length xs = length ys →
+  (∀ i, i < length xs → xs !!! i = ys !!! i) →
+  xs = ys.
+Proof.
+  intros. apply list_eq_same_length'; [ eauto | intros ].
+  rewrite !list_lookup_lookup_total_lt by lia.
+  f_equal. eauto.
 Qed.
 
 (* The tactic [listx i] proves that two lists are equal by checking that
@@ -170,6 +228,21 @@ Qed.
 
 Global Ltac listx i :=
   eapply list_eq_same_length'; list; [ eauto with lia | intros i ? ].
+
+Global Ltac listx_total i :=
+  eapply list_eq_same_length'_total; list; [ eauto with lia | intros i ? ].
+
+(* The tactic [lookup_insert_split] performs a case split in a situation where
+   the goal contains a lookup in an update [<[i:=x]>xs !! j]. *)
+
+Global Ltac lookup_insert_split :=
+  rewrite list_lookup_insert; list; case_decide; list; eauto 2.
+
+(* The tactic [lookup_app_split] performs a case split in a situation where
+   the goal contains a lookup in a concatenation [(xs ++ ys) !! i]. *)
+
+Global Ltac lookup_app_split :=
+  rewrite lookup_app'; list; case_decide; list; eauto 2.
 
 (* -------------------------------------------------------------------------- *)
 
@@ -194,7 +267,7 @@ Global Notation final_seg j xs :=
    of the list [xs]. *)
 
 Global Notation valid_seg i j xs :=
-  (0 ≤ i ≤ j ≤ length xs).
+  (i ≤ j ≤ length xs).
 
 (* [valid i xs] means that [i] is a valid index into the list [xs].
    In other words, this index can be used for reading or updating
@@ -232,7 +305,10 @@ Proof. unfold seg. listx j. eauto. Qed.
 
 (* An empty segment and a complete segment can be simplified away. *)
 
-Lemma seg_none {A} i j (xs : list A) : j ≤ i → seg i j xs = [].
+Lemma seg_none {A} i (xs : list A) : seg i i xs = [].
+Proof. intros. unfold seg. list. eauto. Qed.
+
+Lemma seg_none' {A} i j (xs : list A) : j ≤ i → seg i j xs = [].
 Proof. intros. unfold seg. list. eauto. Qed.
 
 Lemma seg_all {A} i j (xs : list A) : i ≤ 0 → length xs ≤ j → seg i j xs = xs.
@@ -275,7 +351,7 @@ Proof. intros. rewrite length_seg. lia. Qed.
 
 Lemma lookup_seg {A} i j (xs : list A) k :
   valid_seg i j xs →
-  0 ≤ k < j - i →
+  k < j - i →
   seg i j xs !! k = xs !! (i + k).
 Proof. intros. unfold seg. list. eauto. Qed.
 
@@ -283,7 +359,7 @@ Proof. intros. unfold seg. list. eauto. Qed.
 
 Lemma lookup_total_seg `{Inhabited A} i j (xs : list A) k :
   valid_seg i j xs →
-  0 ≤ k < j - i →
+  k < j - i →
   seg i j xs !!! k = xs !!! (i + k).
 Proof.
   intros. rewrite !list_lookup_total_alt, lookup_seg by eauto. eauto.
@@ -294,6 +370,7 @@ Global Hint Rewrite
   @lookup_seg
   @lookup_total_seg
   @seg_none
+  @seg_none'
   @seg_all
   using (list; lia)
 : list.
@@ -305,32 +382,176 @@ Lemma split_seg {A} j (xs : list A) i k :
   valid_seg j k xs →
   seg i k xs = seg i j xs ++ seg j k xs.
 Proof.
-  intros. listx o.
-  rewrite lookup_app'. list. case_decide; list; eauto.
-  f_equal. lia.
+  intros. listx o. lookup_app_split. f_equal. lia.
 Qed.
 
-(* TODO do we want this? *)
+(* A singleton segment is a singleton. *)
+
+Lemma seg_is_singleton `{Inhabited A} i j (xs : list A) :
+  valid i xs →
+  i + 1 = j →
+  seg i j xs = {[xs !!! i]}.
+Proof.
+  intros. subst. listx_total k. assert (k = 0) by lia. subst. list. eauto.
+Qed.
+
+Lemma singleton_is_seg `{Inhabited A} i (xs : list A) :
+  valid i xs →
+  {[xs !!! i]} = seg i (i+1) xs.
+Proof.
+  intros. rewrite seg_is_singleton by eauto. eauto.
+Qed.
+
+(* Automatic recognition of singleton segments
+   and automatic joining of adjacent segments. *)
+
+(* Unfortunately, automatic joining of adjacent segments often requires
+   reasoning up to associativity of [++], so the tactic [list] will miss
+   some opportunities. Manual rewriting using [aac_rewrite <- split_seg]
+   may be necessary. *)
+
+(* TODO do we want this?  *)
+
+Global Hint Rewrite
+  @singleton_is_seg
+  using (list; lia)
+: list.
+
 Global Hint Rewrite
   <- @split_seg
   using (list; lia)
 : list.
 
+(* Interaction of [seg] and [app]. *)
+
+Lemma seg_app {A} i j (xs ys : list A) :
+  seg i j (xs ++ ys) =
+    let n := length xs in
+    seg i j xs ++ seg (i - n) (j - n) ys.
+Proof.
+  intros. unfold seg. list. f_equal. listx k. list. eauto. (* yes! *)
+  (* This is just as good as an SMT solver...! *)
+Qed.
+
+Global Hint Rewrite
+  @seg_app
+  using (list; lia)
+: list.
+
+(* Interaction of [seg] and [insert]. *)
+
+(* Taking a segment out of the result of an update. *)
+
+Lemma seg_insert_outside {A} (xs : list A) i j k x :
+  valid_seg i j xs →
+  ¬ (i ≤ k < j) →
+  seg i j (<[k:=x]>xs) = seg i j xs.
+Proof.
+  intros. listx o. list. eauto.
+Qed.
+
+Lemma seg_insert_inside {A} (xs : list A) i j k x :
+  valid_seg i j xs →
+  i ≤ k < j →
+  seg i j (<[k:=x]>xs) = <[k-i:=x]>(seg i j xs).
+Proof.
+  intros. listx o. list. lookup_insert_split.
+Qed.
+
+Lemma seg_insert {A} (xs : list A) i j k x :
+  valid_seg i j xs →
+  seg i j (<[k:=x]>xs) =
+    if decide (i ≤ k < j) then <[k-i:=x]>(seg i j xs)
+    else seg i j xs.
+Proof.
+  intros. case_decide; eauto using seg_insert_inside, seg_insert_outside.
+Qed.
+
+Global Hint Rewrite
+  @seg_insert_inside
+  @seg_insert_outside
+  using (list; lia)
+: list.
+
+(* Updating a segment. *)
+
+Lemma insert_seg_first {A} i j x (xs : list A) :
+  valid_seg i j xs →
+  i < j →
+  <[0 := x]> (seg i j xs) =
+  {[x]} ++ seg (i+1) j xs.
+Proof.
+  intros. listx o. lookup_app_split. f_equal. lia.
+Qed.
+
+Lemma insert_seg_last {A} i j k x (xs : list A) :
+  valid_seg i j xs →
+  i < j →
+  k = j - i - 1 →
+  <[k := x]> (seg i j xs) =
+  seg i (j - 1) xs ++ {[x]}.
+Proof.
+  intros. listx o. lookup_app_split.
+Qed.
+
+Global Hint Rewrite
+  @insert_seg_first
+  @insert_seg_last
+  using (list; lia)
+: list.
+
+(* Interaction of [seg] and [replicate]. *)
+
+Lemma seg_replicate {A} n i j (x : A) :
+  j ≤ n →
+  seg i j (replicate n x) = replicate (j - i) x.
+Proof.
+  intros.
+  assert (i ≤ j ∨ j < i) as [|] by lia.
+  + listx k. list. eauto.
+  + list. eauto.
+Qed.
+
+(* Interaction of [seg] with itself. *)
+
+Lemma seg_seg {A} i j k l (xs : list A) :
+  valid_seg k l xs →
+  i ≤ j ≤ l - k → (* valid_seg i j (seg k l xs) *)
+  seg i j (seg k l xs) =
+  seg (k + i) (k + j) xs.
+Proof.
+  intros. listx o. list. f_equal. lia.
+Qed.
+
+Global Hint Rewrite
+  @seg_replicate
+  @seg_seg
+  using (list; lia)
+: list.
+
+(* The tactic [split_seg j xs] splits a list [xs] or a list segment
+   [seg i k xs] at index [j]. *)
+
+Global Ltac split_seg j xs :=
+  first [
+    (* case: [xs] is already a segment *)
+    rewrite (split_seg j xs) by (list; lia)
+  | (* case: introduce a list segment first *)
+    rewrite (seg_intro xs); list;
+    rewrite (split_seg j xs) by (list; lia)
+  ];
+  list.
+
 (* -------------------------------------------------------------------------- *)
 
 (* We define [sub i n xs] as the segment of the list [xs] whose start
-   index is [i] and whose length is [n]. It can be considered as a
-   short-hand for [seg i (i + n) xs] and for [take n (drop i xs)]. *)
+   index is [i] and whose length is [n]. *)
 
-Definition sub {A} i n (xs : list A) : list A :=
-  seg i (i + n) xs.
-
-Lemma sub_seg {A} i n (xs : list A) :
-  sub i n xs = seg i (i + n) xs.
-Proof. reflexivity. Qed.
+Global Notation sub i n xs :=
+  (seg i (i + n) xs).
 
 Lemma sub_take_drop {A} i n (xs : list A) :
   sub i n xs = take n (drop i xs).
 Proof.
-  intros. unfold sub, seg. f_equal. lia.
+  intros. unfold seg. f_equal. lia.
 Qed.
