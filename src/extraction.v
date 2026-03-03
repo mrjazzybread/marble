46d07d55a688e23943d8a1592fb1a8eae342b080
François Pottier <francois.pottier@inria.fr>
2026-03-03 10:19:32 +0100

    Disable extraction for now.

--- src/extraction.v
+++ src/extraction.v
@@ -8,8 +8,10 @@ Extraction Inline bind.
 
 From Stdlib Require ExtrOCamlInt63.
 From array Require Import int.
-Recursive Extraction down.
+
+(* Recursive Extraction down up interruptible_up interruptible_up_unit. *)
 
 From Stdlib Require ExtrOCamlPArray.
 From array Require Import array specialization.
-Recursive Extraction of_list blit blit' copy find_index equal.
+
+(* Recursive Extraction of_list blit blit' copy find_index equal. *)
