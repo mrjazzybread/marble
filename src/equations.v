b4c9a7cca89ffca79ab1cc662d52d83830ff1732
François Pottier <francois.pottier@inria.fr>
2026-03-22 18:53:15 +0100

    equations.v: [inspect] is not needed in the definition of [IF/THEN/ELSE].

--- src/equations.v
+++ src/equations.v
@@ -1,9 +1,6 @@
 From Stdlib Require Import Utf8.
 From Stdlib Require Export Wellfounded.Wellfounded.
 From Equations Require Export Equations.
-From Equations.Prop Require Import Logic. (* [inspect] *)
-  (* If [x] has type [A] then the type of [inspect x] is
-     {y : A | x = y}. *)
 
 (* The following notation offers syntactic sugar for a "rich"
    conditional construct. In the first branch, the hypothesis
@@ -13,15 +10,14 @@ From Equations.Prop Require Import Logic. (* [inspect] *)
 
 Global Notation "'IF' e0 'THEN' e1 'ELSE' e2" :=
   (
-    let (b, p) := inspect (e0) in
     (
-      if b as b
+      if e0 as b
       return e0 = b → _
       then
         λ (_ : e0 = true), e1
       else
         λ (_ : e0 = false), e2
-    ) p
+    ) eq_refl
   )
   (at level 70).
 
