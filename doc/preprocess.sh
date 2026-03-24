6825ab0ef583537b1a24fda31bf9bff203c9ff00
François Pottier <francois.pottier@inria.fr>
2026-03-24 19:32:54 +0100

    Preliminary set up to write documentation with links to the code.

---
+++ doc/preprocess.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+
+# Usage: ./preprocess.sh <filename>
+
+# This script preprocesses the file <filename>. It interprets my custom syntax
+# for links and replaces it with correct Markdown links. The result is printed
+# on stdout. There can be at most one custom link per line.
+
+REPO=https://gitlab.inria.fr/fpottier/marble/-/blob/main/src/
+SRC=../src
+
+while IFS= read -r line; do
+  # Identify the custom link syntax: a (short) file name and a pattern,
+  # separated with a colon, surrounded with parentheses. An example is
+  # (array.v:Lemma wp_make)
+  if [[ $line =~ ^(.*)\(([^:]*):([^\)]*)\)(.*)$ ]]; then
+    leading="${BASH_REMATCH[1]}"
+    file="${BASH_REMATCH[2]}"
+    pattern="${BASH_REMATCH[3]}"
+    trailing="${BASH_REMATCH[4]}"
+    # Search for this pattern in this file, and extract the line
+    # number of the match. If there are multiple matches, keep only
+    # the first match. Construct a URL for this particular file and
+    # line number in the repository.
+    number=$(grep -n "${pattern}" "${SRC}"/"${file}" | head -n 1 | cut -f 1 -d ':')
+    URL="${REPO}${file}?ref_type=heads#${number}"
+    # Print the transformed line, where the trailing and leading material
+    # is preserved and a link is printed in the middle.
+    echo "${leading}(${URL})${trailing}"
+  else
+    echo "$line"
+  fi
+done < "$1"
