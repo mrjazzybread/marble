002cb2b13abfda60305a36eb16bfc4f15d0c2db6
François Pottier <francois.pottier@inria.fr>
2026-03-24 15:47:19 +0100

    Split off list_tactics.v.

--- src/sort.v
+++ src/sort.v
@@ -4,7 +4,9 @@ From Stdlib Require Import Array.PArray.
 From Stdlib Require Import Sorting.Permutation Sorting.Sorted.
 From Corelib Require Import Classes.RelationClasses.
 From marble Require Import equations.
-From marble Require Import tactics list_extra iteration int wp wp_tactics array orders sorting compare.
+From marble Require Import tactics list_extra list_tactics.
+From marble Require Import iteration int wp wp_tactics array.
+From marble Require Import orders sorting compare.
 Implicit Types _i _j _k : int.
 
 Unset Universe Minimization ToSet.
@@ -17,43 +19,6 @@ Open Scope nat_scope.
    https://rocq-prover.org/doc/V9.1.0/corelib/Corelib.Classes.RelationClasses.html
    https://rocq-prover.org/doc/v9.0/stdlib/Stdlib.Sorting.Sorted.html
  *)
-
-(* -------------------------------------------------------------------------- *)
-
-(* A few local tactics. *)
-
-(* The tactic [recognize_named_lookups] detects hypotheses of the form
-   [x = xs !!! i] and uses them to replace [xs !!! i] with [x] both in
-   the goal and in all hypotheses. *)
-
-Local Ltac recognize_named_lookups :=
-  repeat match goal with h: ?x = ?xs !!! ?i |- _ =>
-    try rewrite <- !h in *;
-    revert h
-  end;
-  intros.
-
-(* The tactic [use_known_permutation] detects a hypothesis of the form
-   [xs ≃ ys], where [xs] occurs in the goal, and uses it to replace [xs]
-   with [ys] in the goal. (Of course this requires the surrounding context
-   to be compatible with such a rewriting step.) *)
-
-Local Ltac use_known_permutation :=
-  match goal with h: Permutation ?xs ?ys |- context[?xs] =>
-    rewrite h
-  end.
-
-(* The tactic [decompose_segment] detects a hypothesis of the form
-   [seg i j xs ++ {[x]} = seg i (j + 1) ys]. It deduces the two
-   equations [seg i j xs = seg i j ys] and [{[x]} = seg j (j + 1) ys]. *)
-
-Local Ltac decompose_segment :=
-  match goal with
-  h: seg ?i ?j ?xs ++ {[?x]} = seg ?i (?j + 1) ?ys |- _ =>
-    rewrite (split_seg j ys) in h by lia;
-    eapply app_inj_1 in h; [| list; lia ];
-    unpack in h
-  end.
 
 (* -------------------------------------------------------------------------- *)
 
