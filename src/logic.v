489716c9426d3976b3ea8c39972387c44c412f91
François Pottier <francois.pottier@inria.fr>
2026-04-04 18:13:59 +0200

    New predicate [on_seg] and new file logic.v.

---
+++ src/logic.v
@@ -0,0 +1,37 @@
+From stdpp Require Import numbers.
+Open Scope Z_scope.
+
+Unset Universe Minimization ToSet.
+Generalizable All Variables.
+Set Universe Polymorphism.
+
+(* -------------------------------------------------------------------------- *)
+
+(* [on_seg i k P] means that [P j] is true of every index [j]
+   in the semi-open interval [i, k). *)
+
+Definition on_seg i k (P : Z → Prop) :=
+  ∀ j, i ≤ j < k → P j.
+
+Lemma on_seg_init i k P : k ≤ i → on_seg i k P.
+Proof. unfold on_seg. lia. Qed.
+
+Lemma on_seg_step_up i k P : on_seg i k P → P k → on_seg i (k+1) P.
+Proof.
+  unfold on_seg. intros.
+  case (decide (j = k)); intros; try subst; eauto with lia.
+Qed.
+
+Lemma on_seg_step_down i k P : on_seg (i+1) k P → P i → on_seg i k P.
+Proof.
+  unfold on_seg. intros.
+  case (decide (j = i)); intros; try subst; eauto with lia.
+Qed.
+
+Global Hint Unfold on_seg : on_seg.
+
+Global Hint Resolve
+  on_seg_init
+  on_seg_step_up
+  on_seg_step_down
+: on_seg.
