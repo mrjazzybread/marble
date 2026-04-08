29efa78f32b981badad55d69f73437e7888a25f5
François Pottier <francois.pottier@inria.fr>
2026-04-08 23:25:04 +0200

    benchmark.v: more comments.

--- src/benchmark.v
+++ src/benchmark.v
@@ -1,4 +1,5 @@
 From stdpp Require Import base.
+From Stdlib Require Import ZArith.
 From Stdlib Require Import Uint63.
 From Stdlib Require Import Array.PArray.
 From listz Require Import listz.
@@ -17,15 +18,13 @@ Module Int.
 End Int.
 Module Sort := Sort Int.
 
-Implicit Types _i : int.
-
 Open Scope uint63.
 
 (* TODO move *)
 Instance Inhabited_int : Inhabited int.
 Proof. constructor. exact 0. Defined.
 
-Definition data _i :=
+Definition data (_i : int) :=
   (10000 - _i)%uint63.
 
 Time Definition a : array int :=
@@ -37,18 +36,27 @@ Time Definition a : array int :=
 Time Definition a' : array int :=
   Eval vm_compute in
   sort a.
-    (*  2000: 5.8 seconds (sometimes also observed less than 3 seconds?) *)
-    (*        native_compute: 0.9 seconds if warm *)
+    (*  2000:  5.8 seconds in switch rocq9 (sometimes less than 3s?) *)
+    (*         4.4 seconds in switch 4.14.2+flambda *)
+    (*        native_compute: 0.9 to 1.2 seconds if warm, 80 seconds if not *)
     (* 10000: 41.7 seconds (wow) *)
+    (* 16000: 52.1 seconds in switch 4.14.2+flambda *)
 
 Time Definition xs : list int :=
   Eval vm_compute in
-  list_init.init 2000 (λ (i : nat), data (Uint63.of_nat i)).
-    (*  2000: 0.18 seconds *)
+  listz.init 16000%Z (λ (i : Z), data (Uint63.of_Z i)).
+    (*  2000: 0.2 seconds in switch rocq9 *)
+    (*  2000: 0.1 seconds in switch 4.14.2+flambda *)
+    (*  2000: 0.5 seconds in switch 4.14.2+flambda with native_compute *)
     (* 10000: 4.4 seconds  *)
+    (* 16000: 6.1 to 6.6 seconds in switch 4.14.2+flambda *)
 
 Time Definition xs' : list int :=
   Eval vm_compute in
   Sort.sort xs.
     (*  2000: between 0.01 and 0.02 seconds *)
     (* 10000: 0.09 seconds *)
+    (* 16000: 0.1 seconds *)
+
+(* Why is it 60x times faster to sort a list than to allocate it? *)
+(* Why is it 500x times faster to sort a list than to sort an array? *)
