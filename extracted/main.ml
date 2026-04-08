2f7ae059e6c123ddeea4db0555afd6391068e9e4
François Pottier <francois.pottier@inria.fr>
2026-04-08 21:57:24 +0200

    Strengthen the benchmark of the extracted code.

--- extracted/main.ml
+++ extracted/main.ml
@@ -1,7 +1,9 @@
 open Printf
 module B = Common.Benchmark
-let run benchmarks =
-  List.iter B.drive_and_display benchmarks
+
+(* The time that we are willing to spend to run one benchmark.
+   The benchmark is repeated as many times as possible during
+   the allotted time. *)
 let quota = "5.0s"
 
 type uint63 = Uint63.t
@@ -9,6 +11,38 @@ let uint63 = Uint63.of_int
 
 let inhabitant =
   0
+
+(* -------------------------------------------------------------------------- *)
+
+(* Several ways of constructing arrays of size [n]. *)
+
+let random_array n : int array =
+  Random.init 42;
+  Array.init n @@ fun _i -> Random.int 32768
+
+let sorted_array n : int array =
+  Array.init n @@ fun i -> i
+
+let reversed_array n : int array =
+  Array.init n @@ fun i -> n - i
+
+type construction =
+  (int -> int array) * string
+
+let random : construction =
+  random_array, "random data"
+
+let sorted : construction =
+  sorted_array, "sorted data"
+
+let reversed : construction =
+  reversed_array, "reversed data"
+
+(* -------------------------------------------------------------------------- *)
+
+(* A benchmark of the code that I have extracted from Rocq, which is placed
+   in the module [Extracted.Make]. The parameter [A] is an implementation of
+   the module [Array]. We have several possible implementations; see below. *)
 
 module Make (A : sig
   type 'a t
@@ -38,14 +72,16 @@ end) = struct
 
   open Extracted.Make(A)
 
-  (* The benchmark: sorting an integer array of [n] random elements. *)
+  (* The benchmark: sorting an integer array of size [n]. *)
 
-  let benchmark n name : B.benchmark =
+  let benchmark n construction (algorithm : string) : B.benchmark =
     let basis = n
-    and name = sprintf "sort (size %d) (%s)" n name
+    and name =
+      sprintf "sorting %s (size %d) (sort/%s)"
+        (snd construction) n algorithm
     and run () =
-      (* Allocate a fresh mutable array of integers. *)
-      let a : int array = Array.init n @@ fun _i -> Random.int 32768 in
+      (* Construct a fresh mutable array of integers. *)
+      let a : int array = (fst construction) n in
       (* Convert it to a persistent array of unsigned integers. *)
       let p = convert a in
       (* The benchmark is to sort the persistent array [p]. *)
@@ -55,30 +91,43 @@ end) = struct
     in
     B.benchmark ~name ~quota ~basis ~run
 
-end (* ArrayExtra *)
+end (* Make *)
 
-(* The reference: sorting using OCaml's standard library. *)
+(* -------------------------------------------------------------------------- *)
 
-let reference n : B.benchmark =
+(* The baseline: the function [Array.sort] in OCaml's standard library. *)
+
+let baseline n construction : B.benchmark =
   let basis = n
-  and name = sprintf "sort (size %d) (OCaml arrays)" n
+  and name =
+    sprintf "sorting %s (size %d) (Stdlib.Array.sort)"
+      (snd construction) n
   and run () =
     (* Allocate a fresh mutable array of integers. *)
-    let a : int array = Array.init n @@ fun _i -> Random.int 32768 in
+    let a : int array = (fst construction) n in
+    (* Convert it to an array of type [uint63 array]. *)
+    let a : uint63 array = Array.map Uint63.of_int a in
     (* The benchmark is to sort the array [a]. *)
     fun () ->
-      Array.sort compare a
+      Array.sort Uint63.compare a
   in
   B.benchmark ~name ~quota ~basis ~run
 
-(* A list of all sorting benchmarks. *)
+(* -------------------------------------------------------------------------- *)
 
-let benchmarks n = [
-  reference n;
-  (let module M = Make(UnsafeArray) in M.benchmark n "UnsafeArray");
-  (let module M = Make(DefensiveArray) in M.benchmark n "DefensiveArray");
-  (let module M = Make(Parray) in M.benchmark n "Parray");
+(* A list of the benchmarks that we want to run. *)
+
+let benchmarks n construction : B.benchmark list = [
+  baseline n construction;
+  (let module M = Make(UnsafeArray) in M.benchmark n construction "UnsafeArray");
+  (let module M = Make(DefensiveArray) in M.benchmark n construction "DefensiveArray");
+  (let module M = Make(Parray) in M.benchmark n construction "Parray");
 ]
+
+let benchmarks n : B.benchmark list =
+  [ random; sorted; reversed ]
+  |> List.map (benchmarks n)
+  |> List.flatten
 
 (* -------------------------------------------------------------------------- *)
 
@@ -94,6 +143,9 @@ let () =
 
 let sort =
   !sort
+
+let run benchmarks =
+  List.iter B.drive_and_display benchmarks
 
 let possibly n (benchmarks : int -> B.benchmark list) =
   if n > 0 then run (benchmarks n)
