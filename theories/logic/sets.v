9949b2388e626cd445020531355258a9faf47355
François Pottier <francois.pottier@inria.fr>
2026-05-28 19:37:57 +0200

    sets.v: a small theory of [cardinal].

--- theories/logic/sets.v
+++ theories/logic/sets.v
@@ -58,7 +58,7 @@ Qed.
 
 Section Sets.
 
-Variable V : Type.
+Context {V : Type}.
 
 Implicit Types vs ws zs : set V.
 
@@ -105,6 +105,89 @@ Lemma double_complement `{!RelDecision (∈@{set V})} vs :
   ⊤ ∖ (⊤ ∖ vs) ≡ vs.
 Proof.
   rewrite difference_difference_r. set_solver.
+Qed.
+
+(* [cardinal vs n] means that the cardinal of the set [vs] is at most [n]. *)
+
+Definition cardinal vs (n : Z) :=
+  ∃ (enum : list V),
+  length enum ≤ Z.to_nat n ∧
+  ∀ v, v ∈ vs → v ∈ enum.
+
+Local Ltac introCardinal enum :=
+  exists enum; split; eauto.
+
+Local Ltac elimCardinal :=
+  match goal with h: cardinal _ _ |- _ =>
+    destruct h as (enum & ? & Hmem)
+  end.
+
+(* The empty set has cardinal 0. *)
+
+Lemma cardinal_empty vs :
+  vs ⊆ ∅ →
+  cardinal vs 0.
+Proof.
+  exists []. eauto with set_solver.
+Qed.
+
+(* Only the empty set has cardinal 0. *)
+
+Lemma cardinal_empty_rev vs :
+  cardinal vs 0 →
+  vs ⊆ ∅.
+Proof.
+  intros. elimCardinal.
+  assert (enum = []) as ->.
+  { apply nil_length_inv. lia. }
+  eauto with set_solver.
+Qed.
+
+Lemma cardinal_empty_iff vs :
+  cardinal vs 0 ↔
+  vs ⊆ ∅.
+Proof.
+  split; eauto using cardinal_empty, cardinal_empty_rev.
+Qed.
+
+(* If a set is nonempty then it cannot have cardinal 0. *)
+
+Lemma cardinal_empty_contradiction v vs :
+  cardinal vs 0 →
+  v ∈ vs →
+  False.
+Proof.
+  rewrite cardinal_empty_iff. set_solver.
+Qed.
+
+(* If a set has cardinal at most [n]
+   then a subset of it has cardinal at most [n]. *)
+
+Lemma cardinal_mono_1 vs ws n :
+  cardinal ws n →
+  vs ⊆ ws →
+  cardinal vs n.
+Proof.
+  intros. elimCardinal. introCardinal enum.
+Qed.
+
+(* If a set has cardinal at most [n]
+   then a strict subset of it has cardinal at most [n-1]. *)
+
+Lemma cardinal_mono_1_strict `{EqDecision V} vs ws n w :
+  cardinal ws n →
+  vs ⊆ ws →
+  w ∉ vs →
+  w ∈ ws →
+  cardinal vs (n - 1).
+Proof.
+  intros. elimCardinal.
+  introCardinal (filter (λ x, x ≠ w) enum).
+  { assert (Hw1: w ∈ enum) by eauto.
+    assert (Hw2: ¬ w ≠ w) by congruence.
+    generalize (length_filter_lt (λ x, x ≠ w) enum w Hw1 Hw2); intro.
+    lia. }
+  { intros. rewrite list_elem_of_filter. eauto with set_solver. }
 Qed.
 
 End Sets.
